14b2e5ee13bf8b64e6f956200802f08e92df990e
Francisco Javier Estrada Martínez <fjestrad@outlook.com>
2024-07-25 13:42:17 +0200

    feat: Change tab names

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,13 +7,13 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Settings</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Settings</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
